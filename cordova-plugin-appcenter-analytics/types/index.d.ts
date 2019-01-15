a114526b277a5aca32b34f6dac3c1ac1cff3a53d
Konstantin Zhidovinov <v-kozhid@microsoft.com>
2019-01-15 16:16:15 +0300

    Added daclarations for Analytics methods.

---
+++ cordova-plugin-appcenter-analytics/types/index.d.ts
@@ -0,0 +1,16 @@
+declare namespace AppCenter {
+  let Analytics: {
+    trackEvent(
+      eventName: string,
+      properties: {},
+      success: () => void,
+      error: (error: string) => void
+    ): void;
+    isEnabled(success: () => void, error: (error: string) => void): void;
+    setEnabled(
+      enabled: boolean,
+      success: () => void,
+      error: (error: string) => void
+    ): void;
+  };
+}
