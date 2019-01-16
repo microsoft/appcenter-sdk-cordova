641eb40e27ce6f3c322079a22527f541dcba972f
Konstantin Zhidovinov <v-kozhid@microsoft.com>
2019-01-16 12:31:35 +0300

    Fixed spaces and hasCrashedInLastSession signature.

--- cordova-plugin-appcenter-analytics/types/index.d.ts
+++ cordova-plugin-appcenter-analytics/types/index.d.ts
@@ -1,18 +1,21 @@
 declare namespace AppCenter {
-  let Analytics: {
-    trackEvent(
-      eventName: string,
-      properties: {},
-      success: () => void,
-      error: (error: any) => void
-    ): void;
+    let Analytics: {
+        trackEvent(
+            eventName: string,
+            properties: {},
+            success: () => void,
+            error: (error: any) => void
+        ): void;
 
-    isEnabled(success: (isEnabled: boolean) => void, error: (error: any) => void): void;
+        isEnabled(
+            success: (isEnabled: boolean) => void,
+            error: (error: any) => void
+        ): void;
 
-    setEnabled(
-      enabled: boolean,
-      success: () => void,
-      error: (error: any) => void
-    ): void;
-  };
+        setEnabled(
+            enabled: boolean,
+            success: () => void,
+            error: (error: any) => void
+        ): void;
+    };
 }
--- cordova-plugin-appcenter-crashes/types/index.d.ts
+++ cordova-plugin-appcenter-crashes/types/index.d.ts
@@ -1,39 +1,39 @@
 interface Attachments {
-  addTextAttachment(title: string, filename: string);
-  addBinaryAttachment(base64string: string, filename: string, mimeType: string);
+    addTextAttachment(title: string, filename: string);
+    addBinaryAttachment(base64string: string, filename: string, mimeType: string);
 }
 
 declare namespace AppCenter {
-  let Crashes: {
-    generateTestCrash(error?: (error: any) => void);
+    let Crashes: {
+        generateTestCrash(error?: (error: any) => void);
 
-    hasCrashedInLastSession(
-      success: () => void,
-      error: (error: any) => void
-    ): boolean;
+        hasCrashedInLastSession(
+            success: (result: boolean) => void,
+            error: (error: any) => void
+        ): void;
 
-    lastSessionCrashReport(
-      success: (report: {}) => void,
-      error: (error: any) => void
-    ): void;
+        lastSessionCrashReport(
+            success: (report: {}) => void,
+            error: (error: any) => void
+        ): void;
 
-    isEnabled(
-      success: (isEnabled: boolean) => void,
-      error: (error: any) => void
-    ): void;
+        isEnabled(
+            success: (isEnabled: boolean) => void,
+            error: (error: any) => void
+        ): void;
 
-    setEnabled(
-      shouldEnable: boolean,
-      success: () => void,
-      error: (error: any) => void
-    );
+        setEnabled(
+            shouldEnable: boolean,
+            success: () => void,
+            error: (error: any) => void
+        );
 
-    process(
-      processorFunction: (
-        attachments: Attachments,
-        success: (response: any) => void
-      ) => void,
-      errorCallback: (error: any) => void
-    ): void;
-  };
+        process(
+            processorFunction: (
+                attachments: Attachments,
+                success: (response: any) => void
+            ) => void,
+            errorCallback: (error: any) => void
+        ): void;
+    };
 }
