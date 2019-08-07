9c74ce71711d2bfc39da51d53a7a5fb1caf53f26
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-07 16:25:37 +0400

    lwm works on android

--- cordova-plugin-generate-low-memory/src/android/jni/lowmemory-jni.cpp
+++ cordova-plugin-generate-low-memory/src/android/jni/lowmemory-jni.cpp
@@ -23,7 +23,7 @@ static std::vector<void*> data;
 void
 Java_com_example_plugin_LowMemoryJni_generateLowMemory(JNIEnv* env,
                                                   jobject thiz) {
-    size_t size = 128 * 1024 * 1024;
+    size_t size = 32 * 1024 * 1024;
     void *buffer = malloc(size);
     memset(buffer, 42, size);
     data.push_back(buffer);
