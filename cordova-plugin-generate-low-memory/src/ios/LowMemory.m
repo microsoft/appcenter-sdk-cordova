ebcf15d6b83c63d3836a306d01c0160084f9b6b5
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-08 13:50:05 +0400

    ios lowmemory files

---
+++ cordova-plugin-generate-low-memory/src/ios/LowMemory.m
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#import "LowMemory.h"
+#include <stdio.h>
+#include <vector>
+
+@interface LowMemory ()
+
+@property NSMutableArray *buffers;
+
+@property size_t allocated;
+
+@end
+
+@implementation LowMemory
+
+static int const blockSize = 256 * 1024 * 1024;
+
+- (instancetype) init {
+  self = [super init];
+  if (self) {
+    _buffers = [NSMutableArray new];
+    _allocated = 0;
+  }
+  return self;
+}
+
+- (void) generateLowMemory
+{
+  const size_t blockSize = 128 * 1024 * 1024;
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 100 * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
+    void *buffer = malloc(blockSize);
+    memset(buffer, 42, blockSize);
+    [self.buffers addObject:[NSValue valueWithPointer:buffer]];
+    self.allocated += blockSize;
+    NSLog(@"Allocated %zu MB", self.allocated / (1024 * 1024));
+    [self generateLowMemory];
+  });
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+  return YES;
+}
+
+@end
