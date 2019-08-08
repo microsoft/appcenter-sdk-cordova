3e31279dd4ffafc2031f298d8543974e1c27cb29
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-08 16:55:02 +0400

    removed test code

--- cordova-plugin-generate-low-memory/src/android/LowMemory.java
+++ cordova-plugin-generate-low-memory/src/android/LowMemory.java
@@ -9,35 +9,12 @@ import android.os.Handler;
 import android.os.Looper;
 import java.util.concurrent.atomic.AtomicInteger;
 import android.util.Log;
-import android.content.ComponentCallbacks2;
-import android.content.Context;
-import android.content.res.Configuration;
 public class LowMemory extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("generateLowMemory")) {
-            Context context = this.cordova.getActivity().getApplicationContext();
-            Log.d("Tag", "register mMemoryWarningListener");
-            ComponentCallbacks2 mMemoryWarningListener = new ComponentCallbacks2() {
-
-                @Override
-               public void onTrimMemory(int level) {
-                Log.d("Tag", "onTrimMemory " + level);
-               }
-
-                @Override
-               public void onConfigurationChanged(Configuration newConfig) {
-               }
-
-                @Override
-               public void onLowMemory() {
-                Log.d("Tag", "onLowMemory");
-               }
-            };
-            context.registerComponentCallbacks(mMemoryWarningListener);
-
             final AtomicInteger i = new AtomicInteger(0);
             final Handler handler = new Handler(Looper.getMainLooper());
             handler.postDelayed(new Runnable() {
