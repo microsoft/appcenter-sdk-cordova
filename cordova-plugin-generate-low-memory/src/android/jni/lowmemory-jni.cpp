2a1f37be91830676fcfe1e1e6a1346db4c35f472
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-08 16:44:06 +0400

    ios & android fixes

--- cordova-plugin-generate-low-memory/src/android/jni/lowmemory-jni.cpp
+++ cordova-plugin-generate-low-memory/src/android/jni/lowmemory-jni.cpp
@@ -4,7 +4,6 @@
 
 #include <jni.h>
 #include <stdio.h>
-#include <string.h>
 #include <vector>
 
 #ifdef __cplusplus
--- cordova-plugin-generate-low-memory/src/ios/LowMemory.m
+++ cordova-plugin-generate-low-memory/src/ios/LowMemory.m
@@ -13,7 +13,7 @@
 
 @implementation LowMemory
 
-static int const blockSize = 256 * 1024 * 1024;
+static size_t const blockSize = 128 * 1024 * 1024;
 
 - (instancetype) init {
   self = [super init];
@@ -26,17 +26,20 @@ static int const blockSize = 256 * 1024 * 1024;
 
 - (void) generateLowMemory :(CDVInvokedUrlCommand *) command
 {
-  const size_t blockSize = 128 * 1024 * 1024;
+  [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]
+                              callbackId:command.callbackId];
+  [self produceLowMemory];
+}
+
+- (void) produceLowMemory {
   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 100 * NSEC_PER_MSEC), dispatch_get_main_queue(), ^{
     void *buffer = malloc(blockSize);
     memset(buffer, 42, blockSize);
     [self.buffers addObject:[NSValue valueWithPointer:buffer]];
     self.allocated += blockSize;
     NSLog(@"Allocated %zu MB", self.allocated / (1024 * 1024));
-    [self generateLowMemory];
+    [self produceLowMemory];
   });
-  [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK]
-                                callbackId:command.callbackId];
 }
 
 + (BOOL)requiresMainQueueSetup
