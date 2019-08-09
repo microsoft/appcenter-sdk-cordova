dcec574a9fd6427707ee54f0c317671fcb0e1ce7
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-09 10:35:32 +0400

    copyright added to LowMemory.js

--- cordova-plugin-generate-low-memory/www/LowMemory.js
+++ cordova-plugin-generate-low-memory/www/LowMemory.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 module.exports = {
     generateLowMemory: function (successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "LowMemory", "generateLowMemory");
