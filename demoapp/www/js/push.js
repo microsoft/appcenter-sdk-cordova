53daa5d33eac42efd92edad7e63786b696703ff3
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-08-12 10:23:06 +0400

    movd push notification listener initialization to deviceready callback

--- demoapp/www/js/push.js
+++ demoapp/www/js/push.js
@@ -1,6 +1,39 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onNotificationReceived(pushNotification) {
+    var message = pushNotification.message;
+    var title = pushNotification.title;
+
+    if (message === null || message === undefined) {
+        title = 'Android background';
+        message = '<empty>';
+    }
+
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+        message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    alert("title = " + title + "; message = " + message)
+}
+
+function updateNotificationListener(isEnabled) {
+    //This is how you can add listener for notifications.
+    if (isEnabled) {
+        AppCenter.Push.addEventListener('notificationReceived', onNotificationReceived); 
+    } else {
+        AppCenter.Push.removeEventListener('notificationReceived', onNotificationReceived); 
+    }
+}
+
+function onDeviceReady() {
+    AppCenter.Push.isEnabled(function (isEnabled) {
+        updateNotificationListener(isEnabled);
+    });
+}
+
 $(document).bind('pageinit', function () {
     var pushEnabled = false;
 
@@ -9,32 +42,13 @@ $(document).bind('pageinit', function () {
 
     var updatePush = function() {
         $("#btn_toggle_push").html(pushEnabled ? ENABLED_LBL : DISABLED_LBL);
+
         //This is how you can add listener for notifications.
-        if (pushEnabled) {
-            AppCenter.Push.addEventListener('notificationReceived', onNotificationReceived); 
-        } else {
-            AppCenter.Push.removeEventListener('notificationReceived', onNotificationReceived); 
-        }
+        updateNotificationListener(pushEnabled);
     }
 
     var errorHandler = function(err) {
         alert("Something went wrong! " + err);
-    }
-
-    var onNotificationReceived = function(pushNotification) {
-        var message = pushNotification.message;
-        var title = pushNotification.title;
-
-        if (message === null || message === undefined) {
-            title = 'Android background';
-            message = '<empty>';
-        }
-
-        if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
-            message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
-        }
-
-        alert("title = " + title + "; message = " + message)
     }
 
     //This is how you can check whether push is enabled.
