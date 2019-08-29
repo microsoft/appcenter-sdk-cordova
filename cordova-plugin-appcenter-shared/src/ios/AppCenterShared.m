6cb8dc047ca8a9115c0f101b4b05e9deaf359aa2
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-08-29 11:48:48 +0300

    Update AppCenterShared.m

--- cordova-plugin-appcenter-shared/src/ios/AppCenterShared.m
+++ cordova-plugin-appcenter-shared/src/ios/AppCenterShared.m
@@ -54,8 +54,6 @@ static MSWrapperSdk * wrapperSdk;
     int logLevelValue = [logLevel intValue];
     if (logLevelValue > 1 && logLevelValue < 8) {
         [MSAppCenter setLogLevel: logLevelValue];
-    } else if (logLevelValue != 0) {
-        MSLogError(@"AppCenter", @"The provided value of the log level is invalid. Log level should be between 2 and 7.");
     }
 
     logUrl = [settings cordovaSettingForKey:@"LOG_URL"];
